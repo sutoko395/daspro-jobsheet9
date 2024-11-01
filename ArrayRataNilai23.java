da1824f4e801b84ab1efe802f6fd20a43d129b3c
sutoko395 <sutokotokotoko@gmail.com>
2024-11-01 09:51:13 +0700

    push ArrayRataNilai23

--- ArrayRataNilai23.java
+++ ArrayRataNilai23.java
@@ -7,20 +7,38 @@ public class ArrayRataNilai23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs= new int [10];
-        double total = 0;
-        double rata2;
-        
-         for(int i=0; i<nilaiMhs.length; i++) {
-            System.out.println("Masukkan Nilai Mahasiswa ke-"+ (i+1)+" : ");
-            nilaiMhs[i]= sc.nextInt();
-         }
-         for(int i=0; i<nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-         }
-         rata2 = total/nilaiMhs.length;
-         System.out.println("Rata-rata nilai = " + rata2);
+        int jumlahMahasiswa;
+        int[] nilaiMhs;
+        double totalLulus = 0;
+        double totalTidakLulus = 0;
+        int jumlahLulus = 0;
+        int jumlahTidakLulus = 0;
+        double rataRataLulus;
+        double rataRataTidakLulus;
 
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jumlahMahasiswa = sc.nextInt();
+        nilaiMhs = new int[jumlahMahasiswa];
+        
+        for (int i = 0; i < nilaiMhs.length; i++) {
+         System.out.print("Masukkan Nilai Mahasiswa ke-" + (i + 1) + ": ");
+         nilaiMhs[i] = sc.nextInt();
+         }
+         for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                jumlahLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                jumlahTidakLulus++;
+            }
+        }
+         rataRataLulus = jumlahLulus > 0 ? totalLulus / jumlahLulus : 0;
+         rataRataTidakLulus = jumlahTidakLulus > 0 ? totalTidakLulus / jumlahTidakLulus : 0;
+ 
+         System.out.println("Rata-rata nilai lulus = " + rataRataLulus);
+         System.out.println("Rata-rata nilai tidak lulus = " + rataRataTidakLulus);
+ 
          sc.close();
     }
 }
