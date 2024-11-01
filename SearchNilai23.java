f585081e8ea12715107770fcd97a331572870e25
sutoko395 <sutokotokotoko@gmail.com>
2024-11-01 10:00:23 +0700

    push searchNilai23

--- SearchNilai23.java
+++ SearchNilai23.java
@@ -1,21 +1,36 @@
-/**
- * SearchNilai23
- */
+import java.util.Scanner;
+
+/** * SearchNilai23 */
 public class SearchNilai23 {
-
     public static void main(String[] args) {
-        int [] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
 
-        for(int i=0; i<arrNilai.length; i++) {
-            if (key==arrNilai[i]) {
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jumlah = sc.nextInt();
+        int[] arrNilai = new int[jumlah];
+
+        for (int i = 0; i < jumlah; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+        int hasil = -1;
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (key == arrNilai[i]) {
                 hasil = i;
                 break;
             }
         }
         System.out.println();
-        System.out.println("Nilai "+key+" ketemu di ideks ke-"+hasil);
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        } else {
+            System.out.println("Nilai " + key + " tidak ditemukan dalam array.");
+        }
         System.out.println();
+
+        sc.close();
     }
 }
