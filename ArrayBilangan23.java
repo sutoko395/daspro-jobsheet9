870e55e959c04de47c38ab868f588c0e4701f936
sutoko395 <sutokotokotoko@gmail.com>
2024-11-01 08:31:30 +0700

    perbarui ArrayBilangan23

--- ArrayBilangan23.java
+++ ArrayBilangan23.java
@@ -10,9 +10,8 @@ public class ArrayBilangan23 {
         bil[2] = -7;
         bil[3] = 17;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }        
     }
 }
